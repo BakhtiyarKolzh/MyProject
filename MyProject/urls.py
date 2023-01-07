e034b9b5f9b32437b86749426f118f7edd7d34ef
bakhtiyar kolzhabai <bakhtiyar.kolzhabai@gmail.com>
2023-01-08 00:05:01 +0600

    2023/01/08

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -1,29 +1,23 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
 from django.http import HttpRequest, HttpResponse
 
-from views_audi import audi, audi_purchase
+from views_audi import audi_purchase
 from views_name import name
 from views_weather import show_weather
+from market.views import show_cars
 
 
 def hello_world(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(
-        """
-        <h3>Hello, DS!</h3>
-        <div style = "font-size: 18px">
-            <a href="/weather/">What is the weather today?</a><br>
-            <a href="/audi/"> Audi center</a><br>
-            <a href="/name"> What is your name?</a>
-        </div>
-        """)
+    return render(request, "index.html")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("hello/", hello_world),
     path("weather/", show_weather),
-    path("audi/", audi),
+    path("audi/", show_cars),
     path("buy_car/<int:id_>", audi_purchase),
     path("name/", name),
 ]
--- views_audi.py
+++ views_audi.py
@@ -1,20 +1,25 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
-orders=[]
+orders = []
 cars = [
     {"id": 1,
+     "available": 2,
      "model": "A6",
      "image": "https://thumb.tildacdn.com/tild3836-3936-4134-a433-353663656330/-/resize/520x/-/format/webp/2019.png"
      },
     {"id": 2,
+     "available": 2,
      "model": "A7",
      "image": "https://thumb.tildacdn.com/tild3962-6335-4139-b962-386636356338/-/resize/520x/-/format/webp/2019.png"
      },
     {"id": 3,
+     "available": 2,
      "model": "A8",
      "image": "https://thumb.tildacdn.com/tild3764-6131-4966-b237-393635633965/-/resize/520x/-/format/webp/2019.png"
      },
     {"id": 4,
+     "available": 2,
      "model": "Q3",
      "image": "https://thumb.tildacdn.com/tild3032-6439-4663-b965-633332653537/-/resize/520x/-/format/webp/2019.png"
      },
@@ -23,35 +28,18 @@ cars = [
 
 def audi(request: HttpRequest) -> HttpResponse:
     global cars
-    cars_cards = ""
-    for car in cars:
-        button=f"""<a href=/buy_car/{car["id"]}>BUY</a>"""
-        for order in orders:
-            if order["car"]==car:
-                button="""<span style="background-color:red; color:black">PURCHASED</span>"""
-        cars_cards += f"""
-        <div style="width: 200px; height: 200px; margin: 20px; background-color: #FAFAFA; text-align: center">
-            <img width=170px src="{car["image"]}" style="margin-top: 20px; margin-bottom:50px">
-            Audi{car["model"]}<br>
-            {button}
-        </div>
-        """
-    if len(cars)==0:
-        cars_cards="Sorry, all the cars are sold, please comes tomorrow"
-
-    return HttpResponse(f"""
-        Audi service<br><br>
-        <div style ="display: flex; flex-flow: row">
-        {cars_cards}
-        </div>
-        """)
+    context = {
+        "cars": cars,
+        "name": "Timur"
+    }
+    return render(request, "cars.html", context)
 
 
-def audi_purchase(request: HttpRequest, id_: int)->HttpResponse:
+def audi_purchase(request: HttpRequest, id_: int) -> HttpResponse:
     global cars
-    cars_with_id_match=[car for car in cars if car["id"]==id_]
-    car=cars_with_id_match[0]
-    if request.method=="POST":
+    cars_with_id_match = [car for car in cars if car["id"] == id_]
+    car = cars_with_id_match[0]
+    if request.method == "POST":
         orders.append({
             "car": car,
             "name": request.POST.get("name"),
@@ -59,23 +47,7 @@ def audi_purchase(request: HttpRequest, id_: int)->HttpResponse:
             "phone": request.POST.get("phone"),
         })
         print(orders)
+        car["available"] -= 1
         return HttpResponseRedirect("/audi")
 
-    return HttpResponse(f"""
-    <h3>Great! You are going to purchase Audi {car["model"]}!</h3>
-    <form method="post">
-        <label for="name">Your name:</label>
-        <input id="name" name="name" required><br>
-        
-        <label for="phone">Your phone:</label>
-        <input id="phone" name="phone" required><br>
-        
-        <label for="email">Your email:</label>
-        <input id="email" name="email" type="email" required><br>
-        
-        <button type="submit">submit</button>
-        
-    </form>
-    
-    
-    """)
+    return render(request, "purchase_form.html", {"car": car})
